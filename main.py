19db93dbb9e5db70dad4642b0a810c6c4703e739
Juanjo Diaz <diazjuanjose@icloud.com>
2024-05-02 14:38:05 -0300

    Tecla windows cuando se deja presionado <>

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from kmk.scanners import DiodeOrientation
 from kmk.keys import KC
 from kmk.kmk_keyboard import KMKKeyboard
+from kmk.modules.holdtap import HoldTap
 import board
 print("Starting")
 
 
 keyboard = KMKKeyboard()
 
+holdtap = HoldTap()
+keyboard.modules.append(holdtap)
+
 _______ = KC.TRNS
+
+LGUI = KC.HT(KC.NUBS, KC.LGUI, prefer_hold=True, tap_interrupted=False, tap_time=300)
 
 keyboard.col_pins = (board.GP0, board.GP1, board.GP2, board.GP3, board.GP4, board.GP5, board.GP6, board.GP7,
                      board.GP8, board.GP9, board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP15,)
@@ -19,7 +25,7 @@ keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
 keyboard.keymap = [
     [
-        _______, _______,  KC.ESC, KC.NUBS,   KC.F4,    KC.G,   KC.F5,    KC.H,   KC.F6, _______, KC.QUOT, _______,   KC.P0, KC.PDOT,   KC.UP, KC.LGUI,
+        _______, _______,  KC.ESC,    LGUI,   KC.F4,    KC.G,   KC.F5,    KC.H,   KC.F6, _______, KC.QUOT, _______,   KC.P0, KC.PDOT,   KC.UP, KC.LALT,
         _______, KC.LSFT,  KC.TAB, KC.CLCK,   KC.F3,    KC.T, KC.BSPC,    KC.Y, KC.RBRC,   KC.F7, KC.LBRC,   KC.P4,   KC.P5,   KC.P6, KC.PPLS, _______,
         KC.LCTL, _______,  KC.GRV,   KC.F1,   KC.F2,   KC.N5,   KC.F9,   KC.N6,  KC.EQL,   KC.F8, KC.MINS,  KC.DEL,  KC.INS, KC.PGUP, KC.HOME, _______,
         _______, _______,   KC.N1,   KC.N2,   KC.N3,   KC.N4,  KC.F10,   KC.N7,   KC.N8,   KC.N9,   KC.N0,  KC.F11,  KC.F12, KC.PGDN,  KC.END, KC.PSCR,
